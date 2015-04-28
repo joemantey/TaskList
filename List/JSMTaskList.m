56635d57db9bd568bd3f98dfde6fc218a6c1ad14
joemantey <joemantey@gmail.com>
2015-04-27 22:28:11 -0400

    finished first round of search methods for taskList class; beginning work on views

--- List/JSMTaskList.m
+++ List/JSMTaskList.m
@@ -11,6 +11,11 @@
 
 @implementation JSMTaskList
 
+
+
+
+#pragma mark - TaskList search methods
+
 //organize task list by due date
 
 -(NSArray *)returnTaskListOrganizedByDateCreated:(NSMutableArray *)taskArray{
@@ -23,6 +28,8 @@
     
     return returnArray;
 }
+
+
 
 //build array of tasks list which has events organized by due date
 
@@ -45,9 +52,73 @@
     return returnArray;
 }
 
+
+
 //build array of tasks for today
 
-//build array of task for tomorrow and on
+-(NSArray *)returnArrayofTodaysTasks:(NSMutableArray *)taskArray{
+    
+    NSArray *returnArray  = [[NSArray alloc]init];
+    
+    NSMutableArray *buildArray =[[NSMutableArray alloc]init];
+
+    for (JSMTask *task in taskArray) {
+        if ([task.dateDue isToday])
+        {
+            [buildArray addObject:task];
+        }
+    }
+    
+    returnArray = buildArray;
+    
+    return returnArray;
+}
+
+
+
+//build array of task for tomorrow
+
+-(NSArray *)returnArrayofTomorrowsTasks:(NSMutableArray *)taskArray{
+    
+    NSArray *returnArray  = [[NSArray alloc]init];
+    
+    NSMutableArray *buildArray =[[NSMutableArray alloc]init];
+    
+    for (JSMTask *task in taskArray) {
+        if (![task.dateDue isTomorrow])
+        {
+            [buildArray addObject:task];
+        }
+    }
+    
+    returnArray = buildArray;
+    
+    return returnArray;
+}
+
+
+
+//build array of tasks which are neither today nor tomorrow
+
+-(NSArray *)returnArrayofTaskLessTodayAndTomorrow:(NSMutableArray *)taskArray{
+    
+    NSArray *returnArray  = [[NSArray alloc]init];
+    
+    NSMutableArray *buildArray =[[NSMutableArray alloc]init];
+    
+    for (JSMTask *task in taskArray) {
+        if (![task.dateDue isToday] && ![task.dateDue isTomorrow])
+        {
+            [buildArray addObject:task];
+        }
+    }
+    
+    returnArray = buildArray;
+    
+    return returnArray;
+}
+
 
 
 @end
+
