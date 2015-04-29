74c7b8ff9f58b9bacbe16822bea2f1fb60454829
joemantey <joemantey@gmail.com>
2015-04-29 10:58:51 -0400

    set up segue to new task view controller

---
+++ List/JSMNewTaskViewController.h
@@ -0,0 +1,13 @@
+//
+//  JSMNewTaskViewController.h
+//  List
+//
+//  Created by Joseph Smalls-Mantey on 4/29/15.
+//  Copyright (c) 2015 Joseph Smalls-Mantey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JSMNewTaskViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+
+@end
