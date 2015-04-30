aa226aa08a8fb5e28f16bdda8290fdbfb9c69a97
joemantey <joemantey@gmail.com>
2015-04-29 20:58:32 -0400

    began work on animating picker cells

--- List/JSMNewTaskViewController.m
+++ List/JSMNewTaskViewController.m
@@ -9,6 +9,7 @@
 #import "JSMNewTaskViewController.h"
 
 @interface JSMNewTaskViewController ()
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (weak, nonatomic) IBOutlet UITextField *nameField;
 @property (weak, nonatomic) IBOutlet UITextField *dueDateField;
 @property (weak, nonatomic) IBOutlet UIDatePicker *dueDatePicker;
@@ -18,6 +19,11 @@
 @property (weak, nonatomic) IBOutlet UIPickerView *categoryPicker;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *prioritySegmentedControl;
 @property (weak, nonatomic) IBOutlet UITextView *noteField;
+
+@property (nonatomic) BOOL dueDatePickerIsShowing;
+@property (nonatomic) BOOL reminderDatePickerIsShowing;
+@property (nonatomic) BOOL listPickerIsShowing;
+
 
 @end
 
@@ -33,6 +39,55 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - TableViewDelegate 
+
+//Constants for static tableview
+#define nameFieldIndex 0
+#define dateDueFieldIndex 1
+#define dateDuePickerIndex 2
+#define reminderFieldIndex 3
+#define reminderPickerIndex 4
+#define listFieldIndex 5
+#define listPickerIndex 6
+#define priorityFieldIndex 7
+#define notesFieldIndex 8
+
+#define fieldCellHeight 64
+#define pickerCellHeight 162
+
+
+//If datePickerIsShowing, set the height of the cell to 164 (the hieght of a date picker). If !datePickerIsShowing, set the height of the cell to 0
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    CGFloat height = fieldCellHeight;
+    
+    if (indexPath.row == dateDuePickerIndex){
+        
+        height = self.dueDatePickerIsShowing ? pickerCellHeight : 0.0f;
+        
+    } else if (indexPath.row == reminderPickerIndex){
+        
+        height = self.reminderDatePickerIsShowing ? pickerCellHeight : 0.0f;
+        
+    }
+    else if (indexPath.row == listPickerIndex){
+        
+        height = self.listPickerIsShowing ? pickerCellHeight : 0.0f;
+        
+    }
+    
+    return height;
+}
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    
+    
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    
+}
 /*
 #pragma mark - Navigation
 
