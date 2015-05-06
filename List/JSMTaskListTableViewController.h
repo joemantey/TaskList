a194fc3174c4e77a7c03b0a318c168034d7128d5
joemantey <joemantey@gmail.com>
2015-05-05 22:50:25 -0400

    deleted first tableview class

---
+++ List/JSMTaskListTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  JSMTaskListTableViewController.h
+//  List
+//
+//  Created by Joseph Smalls-Mantey on 5/5/15.
+//  Copyright (c) 2015 Joseph Smalls-Mantey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JSMTaskListTableViewController : UITableViewController
+
+@end
