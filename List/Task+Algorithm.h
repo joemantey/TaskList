76b5330a8c7ffee7a9f8620ee459d8ed77724b31
joemantey <joemantey@gmail.com>
2015-06-18 11:08:53 -0400

    created task+algorithm category

---
+++ List/Task+Algorithm.h
@@ -0,0 +1,13 @@
+//
+//  Task+Algorithm.h
+//  List
+//
+//  Created by Joseph Smalls-Mantey on 6/18/15.
+//  Copyright (c) 2015 Joseph Smalls-Mantey. All rights reserved.
+//
+
+#import "Task.h"
+
+@interface Task (Algorithm)
+
+@end
---
+++ List/Task+Algorithm.m
@@ -0,0 +1,13 @@
+//
+//  Task+Algorithm.m
+//  List
+//
+//  Created by Joseph Smalls-Mantey on 6/18/15.
+//  Copyright (c) 2015 Joseph Smalls-Mantey. All rights reserved.
+//
+
+#import "Task+Algorithm.h"
+
+@implementation Task (Algorithm)
+
+@end
